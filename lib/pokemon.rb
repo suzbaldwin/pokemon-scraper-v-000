c41a51e1cfbea7fdba2382810f5690eb711666c2
suzy baldwin <gbaldwin24@comcast.net>
2018-02-02 15:58:52 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,24 @@
 class Pokemon
+  attr_accessor :name, :type, :db, :id
+
+  def initialize(name:, type:, db:, id:)
+    @name = name
+    @type = type
+    @db = db
+    @id = id
+  end
+
+  def self.save(name, type, db)
+    db.execute("INSERT INTO pokemon (name, type) VALUES (?,?)", name, type)
+
+  end
+
+  def self.find(id, db)
+    
+    db.execute("SELECT * FROM pokeman WHERE id = ?", id)
+
+
+
+  end
+
 end
